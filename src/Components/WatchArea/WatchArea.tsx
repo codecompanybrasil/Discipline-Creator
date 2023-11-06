7f12678a25efc1d67c33b3b428e38025e383190a
Pablo Pereira <pablopereira27@gmail.com>
2023-11-06 10:03:58 -0300

    Update WatchArea.tsx
    - Removendo opção de travar/destravar edição do textarea.
    - Substituição de textarea pela tag pre.
    - Incluido uso do navigator clipboard como alternativa ao commandExec Copy que está deprecated.

--- src/Components/WatchArea/WatchArea.tsx
+++ src/Components/WatchArea/WatchArea.tsx
@@ -11,15 +11,22 @@ type WatchAreaProps = {
 }
 
 function WatchArea({ disciplineObject }: WatchAreaProps) {
-    const textAreaRef = useRef(null)
-    const [textDestravar, setTextDestravar] = useState<("Destravar" | "Travar")>("Destravar")
-    const [textAreaReadOnly, setTextAreaReadOnly] = useState<boolean>(true)
-
     const onCopyButtonClick = () => {
-        if (textAreaRef.current) {
-            textAreaRef.current.select();
-            document.execCommand('copy');
-            window.getSelection()?.removeAllRanges()
+        const jsonData = JSON.stringify(disciplineObject);
+
+        if (navigator && navigator.clipboard && navigator.clipboard.writeText) {
+            navigator.clipboard.writeText(jsonData);
+        } else {
+            var dummy = document.createElement("input");
+            dummy.type = 'hidden';
+
+            dummy.value = jsonData;
+
+            document.body.appendChild(dummy);
+            dummy.select();
+            document.execCommand("copy");
+
+            document.body.removeChild(dummy);
         }
     }
 
@@ -33,28 +40,15 @@ function WatchArea({ disciplineObject }: WatchAreaProps) {
         URL.revokeObjectURL(url);
     }
 
-    const handleDestravar = () => {
-        if (textAreaReadOnly) {
-            setTextDestravar("Travar")
-            setTextAreaReadOnly(false)
-        } else {
-            setTextDestravar("Destravar")
-            setTextAreaReadOnly(true)
-        }
-    }
-
     return (
         <div className={styles.watchAreaContainer}>
-            <textarea
-                ref={textAreaRef}
-                value={JSON.stringify(disciplineObject, null, 4)}
-                readOnly={textAreaReadOnly}
-                className={styles.textArea + ' form-control'}
-                style={{ resize: "none" }}></textarea>
+            <pre id="jsonData" className={styles.textArea + ' form-control'}>
+                {JSON.stringify(disciplineObject, null, 4)}
+            </pre>
+
             <div className={styles.button_area}>
                 <DcpButton onClick={onCopyButtonClick} text="Copiar"></DcpButton>
                 <DcpButton onClick={handleDownloadJSON} text="Download"></DcpButton>
-                <DcpButton onClick={handleDestravar} text={textDestravar}></DcpButton>
             </div>
         </div>
     )
